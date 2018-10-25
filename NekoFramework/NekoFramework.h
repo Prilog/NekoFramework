1455765ef393f5ba9f87e9872705798c0ed8a4e2
Семен Трапезников <sementrapeznikov@macbook-air-semen.local>
2018-10-25 17:49:02 +0300

    Initial Commit

---
+++ NekoFramework/NekoFramework.h
@@ -0,0 +1,19 @@
+//
+//  NekoFramework.h
+//  NekoFramework
+//
+//  Created by Семен Трапезников on 25.10.2018.
+//  Copyright © 2018 Prilog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NekoFramework.
+FOUNDATION_EXPORT double NekoFrameworkVersionNumber;
+
+//! Project version string for NekoFramework.
+FOUNDATION_EXPORT const unsigned char NekoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NekoFramework/PublicHeader.h>
+
+
